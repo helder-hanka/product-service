1f144e72aae92134f8fae548a7fa4f6f990de091
helder-hanka <55658803+helder-hanka@users.noreply.github.com>
2025-06-04 15:56:11 +0200

    Ajout d'accolade ouvert de la class
    Signed-off-by: Helder <hfernandes238@gmail.com>

--- src/main/java/com/ff/products_service/controller/ProductController.java
+++ src/main/java/com/ff/products_service/controller/ProductController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-public class ProductController 
+public class ProductController {
     private final ProductService productService;
     public ProductController(ProductService productService) {
 
