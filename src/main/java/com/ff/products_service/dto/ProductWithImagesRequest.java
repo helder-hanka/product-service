900f073a537f4c7729f45eb3e19baeac24498fcc
Helder <hfernandes238@gmail.com>
2025-06-05 17:17:01 +0200

    Changement de nom DB
    Signed-off-by: Helder <hfernandes238@gmail.com>

--- src/main/java/com/ff/products_service/dto/ProductWithImagesRequest.java
+++ src/main/java/com/ff/products_service/dto/ProductWithImagesRequest.java
@@ -2,20 +2,24 @@ package com.ff.products_service.dto;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Data
 public class ProductWithImagesRequest {
+    private Long id;
     private String name;
     private String description;
-    private String price;
+    private BigDecimal price;
     private int stock;
-    private List<String> images;
+    private List<ImageRequest> images;
 
     @Data
     public static class ImageRequest {
+        private Long id;
         private String url;
         private String title;
-        private String isMain;
+        private boolean isMain;
+        private int position;
     }
 }
