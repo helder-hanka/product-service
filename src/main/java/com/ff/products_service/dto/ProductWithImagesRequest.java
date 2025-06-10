9815be52d1c7dfb4d60b4c4a8d3abdf522530bab
Helder <hfernandes238@gmail.com>
2025-06-10 10:56:52 +0200

    Ajout ::Valid images
    Signed-off-by: Helder <hfernandes238@gmail.com>

--- src/main/java/com/ff/products_service/dto/ProductWithImagesRequest.java
+++ src/main/java/com/ff/products_service/dto/ProductWithImagesRequest.java
@@ -1,5 +1,6 @@
 package com.ff.products_service.dto;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -18,6 +19,7 @@ public class ProductWithImagesRequest {
     private BigDecimal price;
     @NotNull(message = "Le stock est obligatoire")
     private int stock;
+    @Valid
     private List<ImageRequest> images;
 
     @Data
