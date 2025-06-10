e8ed1ac20ac52642f2f78f99682b8b28d97b206f
Helder <hfernandes238@gmail.com>
2025-06-10 08:17:56 +0200

    Création class ProductWithImagesRequest
    Signed-off-by: Helder <hfernandes238@gmail.com>

--- src/main/java/com/ff/products_service/dto/ProductWithImagesRequest.java
+++ src/main/java/com/ff/products_service/dto/ProductWithImagesRequest.java
@@ -1,5 +1,7 @@
 package com.ff.products_service.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -7,19 +9,26 @@ import java.util.List;
 
 @Data
 public class ProductWithImagesRequest {
-    private Long id;
+    @NotBlank
+    @NotBlank(message = "Le Nom est obligatoire")
     private String name;
+    @NotBlank(message = "La déscription est obligatoire")
     private String description;
+    @NotNull(message = "Le prix est obligatoire")
     private BigDecimal price;
+    @NotNull(message = "Le stock est obligatoire")
     private int stock;
     private List<ImageRequest> images;
 
     @Data
     public static class ImageRequest {
-        private Long id;
+        @NotBlank
         private String url;
+        @NotBlank
         private String title;
+        @NotNull
         private boolean isMain;
-        private int position;
+        @NotNull
+        private boolean toDelete;
     }
 }
