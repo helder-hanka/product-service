f0481846d4513594a6d71cf7b721cd8107a2d1fa
Helder <hfernandes238@gmail.com>
2025-06-25 08:28:59 +0200

    Mise en place d'Eureka
    Signed-off-by: Helder <hfernandes238@gmail.com>

---
+++ src/main/java/com/ff/products_service/config/FeignClientInterceptor.java
@@ -0,0 +1,28 @@
+package com.ff.products_service.config;
+
+
+import feign.RequestInterceptor;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Configuration
+@RequiredArgsConstructor
+public class FeignClientInterceptor {
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (attributes != null) {
+                HttpServletRequest request = attributes.getRequest();
+                String authorizationHeader = request.getHeader("Authorization");
+                if (authorizationHeader != null && !authorizationHeader.isEmpty()) {
+                    requestTemplate.header("Authorization", authorizationHeader);
+                }
+            }
+        };
+    }
+}
